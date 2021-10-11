62a0367bc2a771e6f2f340e70d81a47d713746da
isnowheart <isnowheart@icloud.com>
2021-10-11 19:19:48 -0300

    Added Get, Post, Put and Delete functions

--- src/domains/developers/DevelopersController.ts
+++ src/domains/developers/DevelopersController.ts
@@ -1,7 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import {
+  StoreDeveloperParams,
+  EditDeveloperParams,
+} from './shared/DeveloperParams';
 import Provider from './Provider';
+import { Developer } from '../../entities/Developer';
 
 @Controller('developers')
 export class DevelopersController {
   constructor(private developerProvider: Provider) {}
+
+  @Get()
+  async index(): Promise<Array<Developer>> {
+    return this.developerProvider.index();
+  }
+
+  @Get(':id')
+  async show(@Param('id') id: number): Promise<Developer> {
+    return this.developerProvider.show(id);
+  }
+
+  @Post()
+  async store(@Body() body: StoreDeveloperParams): Promise<Developer> {
+    return this.developerProvider.store(body);
+  }
+
+  @Put(':id')
+  async edit(
+    @Param('id') id: number,
+    @Body() body: EditDeveloperParams,
+  ): Promise<Developer> {
+    return this.developerProvider.edit(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id') id: number): Promise<void> {
+    return this.developerProvider.delete(id);
+  }
 }
--- src/domains/developers/Provider.ts
+++ src/domains/developers/Provider.ts
@@ -1,8 +1,66 @@
 import {
+  BadRequestException,
+  HttpException,
   Injectable,
+  InternalServerErrorException,
+  NotFoundException
 } from '@nestjs/common';
+import {
+  StoreDeveloperParams,
+  EditDeveloperParams, calcAge
+} from './shared/DeveloperParams';
+import { Developer } from '../../entities/Developer';
 
 
 @Injectable()
 export default class Provider {
+  async index(): Promise<Array<Developer>> {
+    const developerList = await Developer.find();
+    return developerList;
+  }
+
+  async show(id: number): Promise<Developer> {
+    try {
+      const getDeveloper = await Developer.findOneOrFail(id);
+      return getDeveloper;
+    } catch (e) {
+      throw new NotFoundException('Developer not found.');
+    }
+  }
+
+  async store(body: StoreDeveloperParams): Promise<Developer> {
+    try {
+      const newDeveloper = Developer.create({ ...body });
+      newDeveloper.age = calcAge(newDeveloper.birthdate);
+      if(newDeveloper.gender.length > 1) throw new BadRequestException('Gender must have just 1 character.');
+      await newDeveloper.save();
+      return newDeveloper;
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
+  }
+
+  async edit(id: number, body: EditDeveloperParams): Promise<Developer> {
+    const updateDeveloper = await Developer.findOne(id);
+    if (!updateDeveloper) throw new NotFoundException('Developer not found.');
+    try {
+      updateDeveloper.setAttributes(body);
+      updateDeveloper.age = calcAge(updateDeveloper.birthdate);
+      if(updateDeveloper.gender.length > 1) throw new BadRequestException('Gender must have just 1 character.');
+      await updateDeveloper.save();
+      return updateDeveloper;
+    } catch (e) {
+      throw new HttpException(e,404);
+    }
+  }
+
+  async delete(id: number): Promise<void> {
+    const deleteDeveloper = await Developer.findOne(id);
+    if (!deleteDeveloper) throw new NotFoundException('Developer not found.');
+    try {
+      await deleteDeveloper.remove();
+    } catch (e) {
+      throw new InternalServerErrorException();
+    }
+  }
 }
