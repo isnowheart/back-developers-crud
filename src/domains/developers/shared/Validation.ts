dab27b86f149f1300c1dcd641325a4a347059c32
isnowheart <isnowheart@icloud.com>
2021-10-12 14:53:28 -0300

    Validation if hobby is empty

--- src/domains/developers/shared/Validation.ts
+++ src/domains/developers/shared/Validation.ts
@@ -7,7 +7,7 @@ export default {
     const age = today.getFullYear() - birthDate.getFullYear();
     const m = today.getMonth() - birthDate.getMonth();
   
-    if( today < birthDate) throw new BadRequestException('Birth date must be a valid date.');
+    if( today < birthDate) throw new BadRequestException('Birth date must be a valid date as YYYY-MM-DD.');
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
     return age;
     }
@@ -22,6 +22,7 @@ export default {
     if(typeof newDeveloper.name !== 'string') throw new BadRequestException('Name must be a string.');
     if(newDeveloper.gender.length > 1) throw new BadRequestException('Gender must have just 1 character.');
     if(!(newDeveloper.gender === 'M' || newDeveloper.gender === 'F')) throw new BadRequestException('Gender must be F or M.');
+    if(newDeveloper.hobby.length == 0 ) throw new BadRequestException('Hobby is empty.');
     if(newDeveloper.birthdate.length == 0 ) throw new BadRequestException('Birth Date is empty.');
   },
 
@@ -33,6 +34,7 @@ export default {
     if(typeof updateDeveloper.name !== 'string') throw new BadRequestException('Name must be a string.');
     if(updateDeveloper.gender.length > 1) throw new BadRequestException('Gender must have just 1 character.');
     if(!(updateDeveloper.gender === 'M' || updateDeveloper.gender === 'F')) throw new BadRequestException('Gender must be F or M.');
+    if(updateDeveloper.hobby.length == 0 ) throw new BadRequestException('Hobby is empty.');
     if(updateDeveloper.birthdate.length == 0 ) throw new BadRequestException('Birth Date is empty.');
   }
 }
